ec03294724270b6b9e1317843ccd96cea53599bb
Jozhua0012 <joshua.melante@unc.edu.ph>
2024-02-07 15:36:25 +0800

    josh

---
+++ script.js
@@ -0,0 +1,23 @@
+gsap.from(".nav", {
+    opacity: 0,
+    duration: 2,
+    ease: "power2.inOut"
+});
+
+gsap.from(".hero", {
+    opacity: 0,
+    duration: 2,
+    ease: "power2.inOut"
+});
+
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.utils.toArray('.section').forEach(section => {
+  ScrollTrigger.create({
+    trigger: section,
+    start: 'top top',
+    pin: true,
+    pinSpacing: false
+  });
+});
