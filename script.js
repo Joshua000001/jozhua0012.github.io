56ece5a70cd618da9d2c3c6c493bfaea31d26561
Jozhua0012 <joshua.melante@unc.edu.ph>
2024-02-08 21:14:25 +0800

    Activity

--- script.js
+++ script.js
@@ -10,6 +10,25 @@ gsap.from(".hero", {
     ease: "power2.inOut"
 });
 
+gsap.registerPlugin(ScrollTrigger);
+
+        gsap.fromTo('.aboutme-container h1, .aboutme-container p', 
+            { opacity: 0, scale: 0.9 },
+            {
+                opacity: 1,
+                scale: 1,
+                duration: 1,
+                scrollTrigger: {
+                    trigger: '.aboutme-container',
+                    start: 'top 80%',
+                    end: 'bottom 60%',
+                    scrub: true
+                }
+            }
+        );
+
+
+
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -19,5 +38,16 @@ gsap.utils.toArray('.section').forEach(section => {
     start: 'top top',
     pin: true,
     pinSpacing: false
+  });
+});
+gsap.utils.toArray('.section.Portfolio').forEach((section, index) => {
+  const w = section.querySelector('.demo-text');
+  const [x, xEnd] = (index % 2) ? ['100%', (w.scrollWidth - section.offsetWidth) * -50] : [w.scrollWidth * -1, 0];
+  gsap.fromTo(w, { x }, {
+      x: xEnd,
+      scrollTrigger: {
+          trigger: section,
+          scrub: 0.5,
+      }
   });
 });
