ee47355b3a9b1d85daf49109debac271ca9b1db4
Jozhua0012 <joshua.melante@unc.edu.ph>
2024-02-28 20:56:28 +0800

    Actic

---
+++ activities/javascript/01/assets/scripts.js
@@ -0,0 +1,27 @@
+let btnmorning = document.getElementById("btn-morning");
+btnmorning.addEventListener('click', function() {
+
+    let FullName = document.getElementById("text-fullname").value;
+     
+    document.getElementById("h1-output").innerText = "Good Afternoon, " + FullName;
+    console.log(Output);
+});
+
+let btnafternoon = document.getElementById("btn-afternoon");
+btnafternoon.addEventListener('click', function() {
+
+    let FullName = document.getElementById("text-fullname").value;
+     
+    document.getElementById("h1-output").innerText = "Good Afternoon, " + FullName;
+    console.log(Output);
+});
+
+
+let btneve = document.getElementById("btn-eve");
+btneve.addEventListener('click', function() {
+
+    let FullName = document.getElementById("text-fullname").value;
+     
+    document.getElementById("h1-output").innerText = "Good Afternoon, " + FullName;
+    console.log(Output);
+});
